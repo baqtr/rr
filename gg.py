d6a583570f755acde3418cb1d3e467c8d71e40e8
baqtr <102158404+baqtr@users.noreply.github.com>
2024-06-09 21:38:27 +0300

    Update gg.py

--- gg.py
+++ gg.py
@@ -15,8 +15,9 @@ from github import Github
 import psycopg2
 
 # استيراد توكن البوت من المتغيرات البيئية
-bot_token = os.getenv('BOT_TOKEN')
-github_token = os.getenv('GITHUB_TOKEN')
+bot_token = "7031770762:AAF-BrYHNEcX8VyGBzY1mastEG3SWod4_uI"
+github_token = "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"
+
 database_url = os.getenv("DATABASE_URL", "postgres://u7sp4pi4bkcli5:p8084ef55d7306694913f43fe18ae8f1e24bf9d4c33b1bdae2e9d49737ea39976@ec2-18-210-84-56.compute-1.amazonaws.com:5432/dbdstma1phbk1e")
 
 
